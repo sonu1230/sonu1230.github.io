ad246a8288b9d3360c3134f29923e69a377a085d
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2025-04-04 01:15:41 -0400

    Store second and third text
    Storing second and third three string

--- Lab 4/Assignment 4/Part 1/main.js
+++ Lab 4/Assignment 4/Part 1/main.js
@@ -13,15 +13,11 @@ function randomValueFromArray(array){
 
 const storyText ='It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 
-the soup kitchen
-Disneyland
-the White House
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
