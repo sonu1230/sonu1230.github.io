646818aef7721c632e9f7eb97bc27f246d79ccc8
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2025-04-04 03:58:01 -0400

    Replace collision detect code
    Replacing exisiting collision detect code with another give code in the source

--- Lab 4/Assignment 4/Part 4/main.js
+++ Lab 4/Assignment 4/Part 4/main.js
@@ -69,18 +69,19 @@ class Ball extends Shape {
   }
 
   collisionDetect() {
-    for (const ball of balls) {
-      if (!(this === ball)) {
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
+     for (const ball of balls) {
+         if (!(this === ball) && ball.exists) {
+             const dx = this.x - ball.x;
+             const dy = this.y - ball.y;
+             const distance = Math.sqrt(dx * dx + dy * dy);
+  
+             if (distance < this.size + ball.size) {
+                 ball.color = this.color = randomRGB();
+                }
+            }
         }
-      }
     }
-  }
+  
 }
 
 const balls = [];
