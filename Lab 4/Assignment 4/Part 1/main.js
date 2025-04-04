4f5af197f1cce447277bad3fd284df768a68f280
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2025-04-04 01:34:12 -0400

    Add two more new string
    Adding 2 more strings with value 94 fahrenheit temperature and 300 pounds weight

--- Lab 4/Assignment 4/Part 1/main.js
+++ Lab 4/Assignment 4/Part 1/main.js
@@ -43,7 +43,8 @@ function result() {
   if(document.getElementById("uk").checked) {
     const weight = `${Math.round(300*0.0714286)} stone`;
     const temperature =  `${Math.round((94-32) * 5/9)} centigrade`;
-
+    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
   }
 
   story.textContent = ;
