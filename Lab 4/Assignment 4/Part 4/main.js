45a578411c628e85b429404bbcf7c30627218d62
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2025-04-04 04:35:44 -0400

    Bringing again evil circle
    Bringing evil circle into program and creating new evil circle. Adding loop for draw, update and collision and call evil circle instance draw, checkbounds and collision detect.

--- Lab 4/Assignment 4/Part 4/main.js
+++ Lab 4/Assignment 4/Part 4/main.js
@@ -170,17 +170,26 @@ while (balls.length < 25) {
   );
 
   balls.push(ball);
+  count++;
+  para.textContent = 'Ball count: ' + count;
 }
+
+const evilBall = new EvilCircle(random(0, width), random(0, height));
 
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
-  }
+      if (ball.exists) {
+          ball.draw();
+          ball.update();
+          ball.collisionDetect();
+        }
+    }
+  evilBall.draw();
+  evilBall.checkBounds();
+  evilBall.collisionDetect();
 
   requestAnimationFrame(loop);
 }
