6a87946f24a32b5e8d27fcddc25b7b637a313061
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2025-04-04 01:10:31 -0400

    First commit for storing text
    Storing first long text

--- Lab 4/Assignment 4/Part 1/main.js
+++ Lab 4/Assignment 4/Part 1/main.js
@@ -1,4 +1,4 @@
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
 
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
@@ -9,9 +9,9 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
 
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+
+const storyText ='It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
 Willy the Goblin
 Big Daddy
