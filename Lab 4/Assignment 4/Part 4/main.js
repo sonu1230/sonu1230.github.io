ff503fcc609deb562f9b94948ac836eb013b139a
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2025-04-04 04:18:04 -0400

    Window event listener
    Adding keydown event listener to the window object

--- Lab 4/Assignment 4/Part 4/main.js
+++ Lab 4/Assignment 4/Part 4/main.js
@@ -90,6 +90,24 @@ class EvilCircle extends Shape {
   
       this.color = "white";
       this.size = 10;
+
+      window.addEventListener("keydown", (e) => {
+          switch (e.key) {
+             case "a":
+                 this.x -= this.velX;
+                 break;
+             case "d":
+                 this.x += this.velX;
+                 break;
+             case "w":
+                 this.y -= this.velY;
+                 break;
+             case "s":
+                 this.y += this.velY;
+                 break;
+            }
+        });
+    }  
   
 
 const balls = [];
