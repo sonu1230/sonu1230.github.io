236db43ae431979a3af1462544a9a7fea7562ec0
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2025-04-04 02:25:17 -0400

    Wiring button
    Wiring up the darken and lighten button

--- Lab 4/Assignment 4/Part 2/scripts/main.js
+++ Lab 4/Assignment 4/Part 2/scripts/main.js
@@ -31,3 +31,16 @@ newImage.addEventListener('click', () => {
 });
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    const currentClass = btn.getAttribute('class');
+
+    if (currentClass === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+    }
+});
