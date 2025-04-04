3faa78ab3b6157437d2ea254a49ef42ea7a8a71f
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2025-04-04 03:49:33 -0400

    Create class shape
    Creating the class shape  but not including size and color

--- Lab 4/Assignment 4/Part 4/main.js
+++ Lab 4/Assignment 4/Part 4/main.js
@@ -18,14 +18,13 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
+class Shape {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
-    this.color = color;
-    this.size = size;
+   
   }
 
   draw() {
